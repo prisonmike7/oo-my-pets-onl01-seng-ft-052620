11bef5f89d2cf2cb0e3ff69f14dcd5211f360969
Michael Harris <mwharris7@gmail.com>
2020-06-10 09:16:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   attr_reader :name, :species
 
-  @@all
+  @@all = []
 
   def initialize(name)
     @name = name
@@ -25,7 +25,7 @@ class Owner
     @@all
   end
 
-  def self.count
+  def self.clear
     @@all.clear
   end
 
