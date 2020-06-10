d0733416ca852a9c89279f68a1c997078331886f
Michael Harris <mwharris7@gmail.com>
2020-06-10 09:20:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,5 +1,5 @@
 class Cat
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
 
   @@all = []
@@ -7,6 +7,7 @@ class Cat
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
     @@all << self
   end
 
